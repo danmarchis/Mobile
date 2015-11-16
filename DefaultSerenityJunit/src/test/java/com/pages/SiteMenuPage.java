285781694868b7eb9463295597a935e581034690
Ioana Hoaghia <ioanahoaghia@yahoo.com>
2015-11-16 17:33:06 +0200

    View Manager Options

--- DefaultSerenityJunit/src/test/java/com/pages/SiteMenuPage.java
+++ DefaultSerenityJunit/src/test/java/com/pages/SiteMenuPage.java
@@ -1,5 +1,20 @@
 package com.pages;
 
-public class SiteMenuPage {
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
 
+@DefaultUrl("http://172.22.4.88:9090/vacation")
+public class SiteMenuPage extends PageObject {
+	
+	@FindBy(css="a[href*='menuItem=inbox']")
+    private WebElementFacade inbox;
+    
+    @FindBy(css="a[href*='menuItem=view-vacations']")
+    private WebElementFacade viewVacations;
+
+    @FindBy(css="a[href*='menuItem=vacation-report']")
+    private WebElementFacade vacationReport;
+	
 }
