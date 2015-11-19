485ec34852b142497b702a8c1ca1187cbfa4bbb2
Marchis Dan <dan.marchis@evozon.com>
2015-11-19 10:03:40 +0200

    NewVacationRequestTest

--- DefaultSerenityJunit/src/test/java/com/features/search/NewVacationRequestTest.java
+++ DefaultSerenityJunit/src/test/java/com/features/search/NewVacationRequestTest.java
@@ -36,15 +36,32 @@ public class NewVacationRequestTest {
     public NewVacationRequestSteps newVacationRequestSteps;
 
     @Test
-    public void new_simple_request() 
+    public void Holiday_request() 
     {
     	logInSteps.is_the_home_page();
     	logInSteps.login_as_tester();
     	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
     	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
     	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
-    	newVacationRequestSteps.SpecialVacationBox();
-    	newVacationRequestSteps.select_SpecialVacationOptionChildBirth();
+    	newVacationRequestSteps.Holiday();
+    	newVacationRequestSteps.CommentClick();
+    	newVacationRequestSteps.enter_CommentBox("Mama are mere");
+    	newVacationRequestSteps.Save();
+        newVacationRequestSteps.verifyMessage("Your request completed successfully.");
+    	
+    }
+
+   
+    @Test
+    public void Vacation_without_payment_request() 
+    {
+   
+    	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
+    	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
+    	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
+    	newVacationRequestSteps.VacationWithoutPayment();
+    	newVacationRequestSteps.enter_DurationBox("mar");
+    	newVacationRequestSteps.enter_InstitutionBox("para");
         newVacationRequestSteps.CommentClick();
     	newVacationRequestSteps.enter_CommentBox("Mama are mere");
     	newVacationRequestSteps.Save();
@@ -52,5 +69,122 @@ public class NewVacationRequestTest {
     	
     }
 
-   } 
+    
+    @Test
+    public void SickLeave_request()
+    {
+   
+    	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
+    	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
+    	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
+    	newVacationRequestSteps.SickLeaveBox();
+        newVacationRequestSteps.CommentClick();
+    	newVacationRequestSteps.enter_CommentBox("Mama are mere");
+    	newVacationRequestSteps.Save();
+        newVacationRequestSteps.verifyMessage("Your request completed successfully.");
+    	
+    }
+
+
+    @Test
+    public void Special_Vacation_Marriage_request()
+    {
+   
+    	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
+    	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
+    	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
+    	newVacationRequestSteps.SpecialVacationBox();
+        newVacationRequestSteps.CommentClick();
+    	newVacationRequestSteps.enter_CommentBox("Mama are mere");
+    	newVacationRequestSteps.Save();
+        newVacationRequestSteps.verifyMessage("Your request completed successfully.");
+    	
+    }
+
+
+    @Test
+    public void Special_Vacation_ChildBirth_request()
+    {
+   
+    	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
+    	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
+    	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
+    	newVacationRequestSteps.SpecialVacationBox();
+        newVacationRequestSteps.select_SpecialVacationOptionChildBirth();
+    	newVacationRequestSteps.CommentClick();
+    	newVacationRequestSteps.enter_CommentBox("Mama are mere");
+    	newVacationRequestSteps.Save();
+        newVacationRequestSteps.verifyMessage("Your request completed successfully.");
+    	
+    }
+
+    @Test
+    public void Special_Vacation_Funeral_request()
+    {
+   
+    	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
+    	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
+    	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
+    	newVacationRequestSteps.SpecialVacationBox();
+        newVacationRequestSteps.select_SpecialVacationOptionFuneral();
+    	newVacationRequestSteps.CommentClick();
+    	newVacationRequestSteps.enter_CommentBox("Mama are mere");
+    	newVacationRequestSteps.Save();
+        newVacationRequestSteps.verifyMessage("Your request completed successfully.");
+    	
+    }
+
+    @Test
+    public void Special_Vacation_Other_request()
+    {
+   
+    	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
+    	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
+    	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
+    	newVacationRequestSteps.SpecialVacationBox();
+        newVacationRequestSteps.select_SpecialVacationOptionOther();
+    	newVacationRequestSteps.CommentClick();
+    	newVacationRequestSteps.enter_CommentBox("Mama are mere");
+    	newVacationRequestSteps.Save();
+        newVacationRequestSteps.verifyMessage("Your request completed successfully.");
+    	
+    }
+
+
+    @Test
+    public void MaternityLeave_Prenatal_Postnalat_request()
+    {
+   
+    	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
+    	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
+    	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
+    	newVacationRequestSteps.MaternityLeaveBox();
+        newVacationRequestSteps.CommentClick();
+    	newVacationRequestSteps.enter_CommentBox("Mama are mere");
+    	newVacationRequestSteps.Save();
+        newVacationRequestSteps.verifyMessage("Your request completed successfully.");
+    	
+    }
+
+    @Test
+    public void MaternityLeave_Concediu_Ingrijire_Copii_request()
+    {
+   
+    	newVacationRequestSteps.click_NewVacationRequestFromTheLeftMenu();
+    	newVacationRequestSteps.selectStartDate(10, "Oct", 2016);
+    	newVacationRequestSteps.selectEndDate(10, "Oct", 2016);
+    	newVacationRequestSteps.MaternityLeaveBox();
+    	newVacationRequestSteps.enter_DurationBox("para");
+    	newVacationRequestSteps.enter_InstitutionBox("mar");
+        newVacationRequestSteps.CommentClick();
+    	newVacationRequestSteps.enter_CommentBox("Mama are mere");
+    	newVacationRequestSteps.Save();
+        newVacationRequestSteps.verifyMessage("Your request completed successfully.");
+    	
+    }
+
+    
+    
+
+} 
 
